8567878451dd68fbdb6dde1d8cb380d31b6d6463
Jhoan López <jhoanlopezclase@gmail.com>
2022-02-04 09:05:17 +0100

    first commit

---
+++ Proyecto I/PrimerProyecto.py
@@ -0,0 +1,17 @@
+#---------------------------------------------------------
+frase = """
+Se utilizan 3 comillas
+para que se imprima 
+en lineas diferentes
+"""
+print(frase)
+#---------------------------------------------------------
+papa = 100
+print (papa)
+#---------------------------------------------------------
+# Comentario de una linea
+""" 
+Y este comentario
+es de varias
+lineas """
+#---------------------------------------------------------
